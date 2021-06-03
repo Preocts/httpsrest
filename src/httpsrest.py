5935259b1b3a14bc2a0eb9493e457d9af8142434
Preocts <preocts@gmail.com>
2021-06-02 21:41:45 -0400

    Initial build (#1)
    * Show missing lines on coverage report
    
    * Initial class creation with url state
    
    * Config dataclass with timeout setter/getter
    
    * Timeout throttle set and get
    
    * Setter and getter for int config values
    
    * add config setters and getters for booleans
    
    * add set/get/remove for retry codes
    
    * Replace dataclasses
    
    * Remove int parser on retry_codes, let them eat cake

--- src/httpsrest.py
+++ src/httpsrest.py
@@ -1,11 +1,143 @@
-"""This is just a sample file"""
+"""
+HTTPS REST API Handler
+
+Author: Preocts <preocts@preocts.com>
+Repo: https/github.com/Preocts/httpsrest
+"""
+import logging
+from typing import MutableSet
+from typing import Tuple
+
+TIMEOUT_DEFAULT = 30
+THROTTLE_TIMEOUT_DEFAULT = 60
+MAX_RETRIES_DEFAULT = 3
+RETRY_ON_DEFAULT = {401, 402, 403, 408, 500, 502, 503, 504}
+ENCODE_URL_DEFAULT = True
+USE_URLENCODE_DEFAULT = False
 
 
-def main() -> bool:
-    """Main"""
-    return True
+class HttpsRestConfig:
+    """Dataclass structure for configuration values"""
+
+    def __init__(self) -> None:
+        """Define defaults"""
+        self.connection_timeout: int = TIMEOUT_DEFAULT
+        self.throttle_timeout: int = THROTTLE_TIMEOUT_DEFAULT
+        self.max_retries: int = MAX_RETRIES_DEFAULT
+        self.retry_on: MutableSet[int] = RETRY_ON_DEFAULT
+        self.encode_url: bool = ENCODE_URL_DEFAULT
+        self.use_urlencode: bool = USE_URLENCODE_DEFAULT
 
 
-def squared(value: int) -> int:
-    """Returns the squared value"""
-    return value * value
+class HttpsRest:
+    """Simple REST calls to APIs"""
+
+    logger = logging.getLogger(__name__)
+
+    def __init__(self, url: str, base_route: str = "") -> None:
+        """url should be domain level only. No routes, no HTTPS://"""
+        self._url = self._parse_url(url)
+        self._base_route = ""
+        self._config = HttpsRestConfig()
+        self.set_base_route(base_route)
+
+    @property
+    def url(self) -> str:
+        """Base url class will use. To update, create new instance"""
+        return self._url
+
+    @property
+    def base_route(self) -> str:
+        """Base route appended to all API calls"""
+        return self._base_route
+
+    @property
+    def timeout(self) -> int:
+        """Time, in seconds, a connection will wait before timing out"""
+        return self._config.connection_timeout
+
+    @property
+    def throttle_timeout(self) -> int:
+        """Time, in seconds, a connection will pause if throttled before continuing"""
+        return self._config.throttle_timeout
+
+    @property
+    def max_retries(self) -> int:
+        """Number of retries that will be attempted before giving up"""
+        return self._config.max_retries
+
+    @property
+    def encode_url(self) -> bool:
+        """If true, all urls are encoded prior to sending"""
+        return self._config.encode_url
+
+    @property
+    def use_urlencode(self) -> bool:
+        """If true, json payloads are translated to URL parameters"""
+        return self._config.use_urlencode
+
+    @property
+    def retry_on(self) -> Tuple[int, ...]:
+        """
+        Returns a tuple of HTTP status codes that will trigger a retry
+
+        NOTE: 429 (throttled) always triggers a retry
+        """
+        return tuple(self._config.retry_on)
+
+    def set_timeout(self, seconds: int = TIMEOUT_DEFAULT) -> None:
+        """Set the time, in seconds, a connection will wait before timing out"""
+        self._config.connection_timeout = self._parse_no_negative_int(seconds)
+
+    def set_throttle_timeout(self, seconds: int = THROTTLE_TIMEOUT_DEFAULT) -> None:
+        """Time, in seconds, a connection will pause if throttled before continuing"""
+        self._config.throttle_timeout = self._parse_no_negative_int(seconds)
+
+    def set_max_retries(self, count: int = MAX_RETRIES_DEFAULT) -> None:
+        """Set the number of retries that will be attempted before giving up"""
+        self._config.max_retries = self._parse_no_negative_int(count)
+
+    def set_encode_url(self, encode_url: bool = ENCODE_URL_DEFAULT) -> None:
+        """If true, all urls will be encoded prior to sending"""
+        self._config.encode_url = bool(encode_url)
+
+    def set_use_urlencode(self, use_urlencode: bool = USE_URLENCODE_DEFAULT) -> None:
+        """If true, json payloads are translated to URL parameters"""
+        self._config.use_urlencode = bool(use_urlencode)
+
+    def set_retry_on_codes(self, *args: int) -> None:
+        """Adds given response codes to set that trigger a retry attemp"""
+        for code in args:
+            self._config.retry_on.add(code)
+
+    def remove_retry_on_codes(self, *args: int) -> None:
+        """Removes given response codes from set that trigger a retry attemp"""
+        for code in args:
+            self._config.retry_on.discard(code)
+
+    def _parse_no_negative_int(self, value: int, default: int = 0) -> int:
+        """Returns value or default if value is less than 0"""
+        if isinstance(value, int):
+            return value if value > default else default
+        else:
+            raise ValueError("Expected int, got %s", type(value))
+
+    def _parse_url(self, url: str) -> str:
+        """Cleans url string, raises if route or query is included"""
+        cleaned_url = url.lower().replace("https://", "").replace("http://", "")
+
+        if "/" in cleaned_url:
+            self.logger.error("Route in url: %s", cleaned_url)
+            raise Exception("Do not include /routes in base url, use .set_base_route()")
+        if "?" in cleaned_url:
+            self.logger.error("Possible ? parameters in url: %s", cleaned_url)
+            raise Exception("Remove URI parameters from base url.")
+
+        return cleaned_url
+
+    def set_base_route(self, base_route: str) -> str:
+        """Set the default route for all calls. Returns stored value."""
+        prefix = "/" if not base_route.startswith("/") else ""
+        post_strip = "/" if base_route.endswith("/") else ""
+        self._base_route = f"{prefix}{base_route.rstrip(post_strip)}"
+        return self.base_route
--- tests/httpsrest_test.py
+++ tests/httpsrest_test.py
@@ -1,20 +1,114 @@
-"""Tests for sample"""
-import httpsrest
+"""Tests for httprest"""
+from typing import Generator
+
 import pytest
+from httpsrest import HttpsRest
 
 
-def test_main() -> None:
-    """Main test"""
-    assert httpsrest.main()
+@pytest.fixture(scope="function", name="base_client")
+def fixture_base_client() -> Generator[HttpsRest, None, None]:
+    """Yields a default init'ed class instance pointed to localhost"""
+    client = HttpsRest("localhost")
+    yield client
 
 
 @pytest.mark.parametrize(
-    ("value_in", "expected"),
+    ("url", "expected", "raised"),
     (
-        (2, 4),
-        (4, 16),
-        (16, 256),
+        ("https://example.com", "example.com", False),
+        ("http://example.com", "example.com", False),
+        ("example.com", "example.com", False),
+        ("https://example.com/api/path", "", True),
+        ("http://example.com/api/path", "", True),
+        ("example.com?limit=100", "", True),
     ),
 )
-def test_squared(value_in: int, expected: int) -> None:
-    assert httpsrest.squared(value_in) == expected
+def test_url_parsing(url: str, expected: str, raised: bool) -> None:
+    """Remove HTTPS:// and HTTP://"""
+    if raised:
+        with pytest.raises(Exception):
+            client = HttpsRest(url)
+
+    else:
+
+        client = HttpsRest(url)
+        assert client.url == expected
+
+
+@pytest.mark.parametrize(
+    ("base_route", "expected"),
+    (
+        ("api/path/", "/api/path"),
+        ("/api/path/", "/api/path"),
+        ("api/path", "/api/path"),
+        ("/api/path", "/api/path"),
+    ),
+)
+def test_set_base_route(base_client: HttpsRest, base_route: str, expected: str) -> None:
+    """Setting base route of all API calls"""
+    base_client.set_base_route(base_route)
+    assert base_client.base_route == expected
+
+
+@pytest.mark.parametrize(
+    ("attrib", "set_", "get_"),
+    (
+        ("timeout", 10, 10),
+        ("timeout", -19, 0),
+        ("timeout", 0, 0),
+        ("timeout", "foo", -1),
+        ("throttle_timeout", 10, 10),
+        ("throttle_timeout", -19, 0),
+        ("throttle_timeout", 0, 0),
+        ("throttle_timeout", "foo", -1),
+        ("max_retries", 10, 10),
+        ("max_retries", -19, 0),
+        ("max_retries", 0, 0),
+        ("max_retries", "foo", -1),
+    ),
+)
+def test_get_set_config_ints(
+    base_client: HttpsRest, attrib: str, set_: int, get_: int
+) -> None:
+    """Tests setters and getters for config items that are ints"""
+    setter = getattr(base_client, f"set_{attrib}")
+    if get_ >= 0:
+        setter(set_)
+        assert getattr(base_client, attrib) == get_
+    else:
+        with pytest.raises(ValueError):
+            setter(set_)
+
+
+@pytest.mark.parametrize(
+    ("attrib", "set_", "get_"),
+    (
+        ("encode_url", True, True),
+        ("encode_url", False, False),
+        ("encode_url", 0, False),
+        ("encode_url", "foo", True),
+        ("use_urlencode", True, True),
+        ("use_urlencode", False, False),
+        ("use_urlencode", 0, False),
+        ("use_urlencode", "foo", True),
+    ),
+)
+def test_get_set_config_bool(
+    base_client: HttpsRest, attrib: str, set_: bool, get_: bool
+) -> None:
+    """Tests setters and getters for config items that are bools"""
+    setter = getattr(base_client, f"set_{attrib}")
+    setter(set_)
+    assert getattr(base_client, attrib) == get_
+
+
+def test_retry_on_set_remove_get(base_client: HttpsRest) -> None:
+    """Tests setter, getter, and remover for retry code set"""
+    start_len = len(base_client.retry_on)
+    base_client.set_retry_on_codes(1, 2, -3, 4)
+    assert len(base_client.retry_on) == start_len + 4
+    assert 4 in base_client.retry_on
+
+    base_client.remove_retry_on_codes(1, 2, -3, 4)
+    assert len(base_client.retry_on) == start_len
+    assert 4 not in base_client.retry_on
